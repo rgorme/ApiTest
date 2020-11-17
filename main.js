4bd30874f219240ec7728339e5830ee4587b75e9
Rasmus Gormsbøl Larsen <RAGLA@brk.dk>
2020-11-17 19:20:23 +0100

    Docker

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const host = "0.0.0.0";
+const port = 3033;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
 
-app.listen(port, () => {
+app.listen(port, host, () => {
     console.log(`Server now listening on port http://localhost:${port}`)
-})
-
-// testing
+});
