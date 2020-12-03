578c526cc99fc168c39b210ce4abfb80abe21f30
Rasmus Gormsbøl Larsen <RAGLA@brk.dk>
2020-12-03 19:08:39 +0100

    proc

--- repository/author/author.repository.js
+++ repository/author/author.repository.js
@@ -3,7 +3,7 @@ var TYPES = require('tedious').TYPES;
 
 function AuthorRepository(dbContext) {
     function getAuthors(req, res) {
-        dbContext.get("getAuthor", function (error, data) {
+        dbContext.get("exec getAuthor", function (error, data) {
             return res.json(response(data, error));
         });
     }
